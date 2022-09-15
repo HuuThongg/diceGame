1c37b7e110287c625dc09c702615584390230171
khuongngo0310 <khuongngo0310@gmail.com>
2022-09-14 20:29:33 -0500

    Add hello page

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const db = require('mongodb');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.send('This is Homepage');
 });
+
 app.get('/login', (req, res) => {
     res.send('This is a login page');
-})
-app.listen(3000, function() {
+});
+
+app.get('/hello/:name', (req, res) => {
+    res.send(`Hello ${req.params.name}`);
+});
+
+const port = process.env.port || 3000;
+app.listen(port, function() {
     console.log('Example app listening on port 3000!');
 });
