e9382b1810beecbfe193e9628b2cf9e478ad99cc
khuongngo0310 <khuongngo0310@gmail.com>
2022-09-14 20:21:49 -0500

    Add login page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 app.get('/login', (req, res) => {
-
+    res.send('This is a login page');
 })
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
